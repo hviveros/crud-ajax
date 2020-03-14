028b5c57d74c1e8af8fcec5cf2ed6da0fd28bbbb
hviveros <humberto.viverosrios@gmail.com>
2020-03-13 22:19:56 -0300

    Se crea la funcion para Actualizar registros

--- views/paginas/inicio.php
+++ views/paginas/inicio.php
@@ -46,8 +46,8 @@
 							<td><?=$r['nombre'];?></td>
 							<td><?=$r['email'];?></td>
 							<td>
-								<a href="?page=editar&id=<?= $r['id']; ?>" type="a" class="btn btn-info">Editar</a>
-								<a href="?page=eliminar&id=<?= $r['id']; ?>" type="a" class="btn btn-danger">Eliminar</a>
+								<button class="btn btn-info" data-toggle="modal" data-target="#editarRegistroModal" onclick="cargarFormEditar(<?= $r['id']; ?>)">Editar</button>
+								<button href="?page=eliminar&id=<?= $r['id']; ?>" class="btn btn-danger">Eliminar</button>
 							</td>
 						</tr>
 						<?php } } ?>
@@ -64,7 +64,7 @@
 
 
 <!-- Modal Agregar -->
-<div class="modal fade" id="insertarRegistroModal" tabindex="-1" role="dialog" aria-labelledby="insetarRegistroModal" aria-hidden="true">
+<div class="modal fade" id="insertarRegistroModal" tabindex="-1" role="dialog" aria-labelledby="insertarRegistroModal" aria-hidden="true">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
@@ -76,7 +76,7 @@
 			<form method="POST" name="registroForm" id="registroForm" class="text-left">
 				<div class="modal-body">
 					<div class="form-group">
-						<label for="nombre">Nombre</label>
+						<label for="nombreU">Nombre</label>
 						<input type="text" id="nombre" name="nombre" class="form-control" aria-describedby="nombreHelp">
 						<small id="nombreHelp" class="form-text text-muted">Ingrese el nombre completo del cliente.</small>
 					</div>
@@ -89,6 +89,40 @@
 				<div class="modal-footer">
 					<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
 					<button type="button" id="btnInsertar" name="btnInsertar" class="btn btn-primary">Insertar</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+
+<!-- Modal Editar -->
+<div class="modal fade" id="editarRegistroModal" tabindex="-1" role="dialog" aria-labelledby="editarRegistroModal" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="editarRegistroModal">Editar registro</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<form id="editarForm" class="text-left">
+				<div class="modal-body">
+					<input type="hidden" id="id" name="id">
+					<div class="form-group">
+						<label for="nombreU">Nombre</label>
+						<input type="text" id="nombreU" name="nombreU" class="form-control" aria-describedby="nombreHelp">
+						<small id="nombreHelp" class="form-text text-muted">Ingrese el nombre completo del cliente.</small>
+					</div>
+					<div class="form-group">
+						<label for="emailU">E-mail</label>
+						<input type="email" id="emailU" name="emailU" class="form-control" aria-describedby="emailHelp">
+						<small id="emailHelp" class="form-text text-muted">Ingrese el correo electronico del cliente.</small>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+					<button type="button" id="btnEditar" name="btnEditar" class="btn btn-info">Editar</button>
 				</div>
 			</form>
 		</div>
@@ -117,8 +151,50 @@
 			});
 
 		});
+
+		$('#btnEditar').click(function(){
+			
+			datos=$('#editarForm').serialize();
+
+			$.ajax({
+				type: "POST",
+				data: datos,
+				url:  "index.php?page=editar",
+				success: function(r){
+					//console.log(datos);
+					$('#tablaCliente').load(' #tablaCliente');
+					// if (r==1) {
+					// 	alert("Editado Ok!");
+					// } else {
+					// 	alert("Fallo al editar");
+					// }
+				}
+			});
+		});
 		
 		$('#tablaCliente').load(' #tablaCliente');
 
 	});
+
+	function cargarFormEditar(id){
+		//alert(id);
+		$.ajax({
+			method: "POST",
+			data: "id=" + id,
+			url: "views/paginas/cargar.php",
+			success: function(r){
+				//console.log(r);
+				//if (r==1) {
+				//alert("Funciona hasta aqui");
+				datos=jQuery.parseJSON(r);
+				$('#id').val(datos['id']);
+				$('#nombreU').val(datos['nombre']);
+				$('#emailU').val(datos['email']);
+				//} else {
+				//alert("Fallo al editar");
+				//}
+			}
+		});
+	}
+
 </script>
