030532ac90322c43750909e0cf8128a5be51b2d1
Humberto Viveros <humberto.viverosrios@gmail.com>
2023-07-14 00:59:15 -0400

    Actualizacón 14-07-23 nuevas versiones

---
+++ models/bd.sql
@@ -0,0 +1,22 @@
+/*
+Elemental: CRUD de clientes con MVC - PDO
+*/
+
+DROP DATABASE IF EXISTS up_crud_ajax;
+
+CREATE DATABASE IF NOT EXISTS up_crud_ajax;
+
+USE up_crud_ajax;
+
+/*tabla cliente*/
+CREATE TABLE cliente(
+    id INT(4) PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+)ENGINE=InnoDB;
+
+/*insersión de datos*/
+INSERT INTO `cliente` (`id`, `nombre`, `email`) VALUES
+(1, 'Tyrion Lannister', 'tyrion@lannisport.com'),
+(2, 'Robb Stark', 'robb@thenorth.com'),
+(3, 'Oberyn Martell', 'oberyn@viper.com');
---
+++ views/includes/cabecera.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>CRUD con PHP - AJAX</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.3.0 -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+
+    <!-- jQuery v3.7.0 -->
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    
+</head>
+<body>
